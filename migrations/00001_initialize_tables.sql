7ac11ac07f07eab0534d075438740ba5606e5990
bazarbru <ruslan.bazarbayev@bcc.kz>
2026-03-18 14:43:30 +0500

    feat: updated events migration

--- migrations/00001_initialize_tables.sql
+++ migrations/00001_initialize_tables.sql
@@ -63,55 +63,55 @@ VALUES ('019cff8f-41e6-79f8-b6ef-728d33f1990a', 'DH5Q3E0J5X2E', '019cff62-d5f7-7
        ('019cff8f-41e8-72c3-8fee-979d16a47ba3', 'DH5PGKRZ4BVX', '019cff62-d5f9-750e-a8b2-744652b8059e', 'San Francisco, CA', 'Las Vegas, NV', 'Maria Garcia', 130000, 104000),
        ('019cff8f-41e8-72cd-b1e4-47b268f143d0', 'DH5PIULEAYDU', '019cff62-d5f9-7516-8dbc-2a03941959dc', 'Detroit, MI', 'Columbus, OH', 'Robert Taylor', 40000, 32000);
 
-INSERT INTO public.events (shipment_id, status, details)
-VALUES ('019cff8f-41e6-79f8-b6ef-728d33f1990a', 0, 'Shipment created.'),
-       ('019cff8f-41e6-79f8-b6ef-728d33f1990a', 1, 'Driver assigned, awaiting pickup in Chicago.'),
-       ('019cff8f-41e6-79f8-b6ef-728d33f1990a', 2, 'Picked up by John Doe.'),
-       ('019cff8f-41e6-79f8-b6ef-728d33f1990a', 3, 'In transit to New York.'),
-       ('019cff8f-41e6-79f8-b6ef-728d33f1990a', 6, 'Successfully delivered to destination.'),
+INSERT INTO public.events (shipment_id, status, details, occurred_at)
+VALUES ('019cff8f-41e6-79f8-b6ef-728d33f1990a', 0, 'Shipment created.', now()),
+       ('019cff8f-41e6-79f8-b6ef-728d33f1990a', 1, 'Driver assigned, awaiting pickup in Chicago.', now() + INTERVAL '1 minute'),
+       ('019cff8f-41e6-79f8-b6ef-728d33f1990a', 2, 'Picked up by John Doe.', now() + INTERVAL '2 minutes'),
+       ('019cff8f-41e6-79f8-b6ef-728d33f1990a', 3, 'In transit to New York.', now() + INTERVAL '3 minutes'),
+       ('019cff8f-41e6-79f8-b6ef-728d33f1990a', 6, 'Successfully delivered to destination.', now() + INTERVAL '4 minutes'),
 
-       ('019cff8f-41e8-71d0-9431-a472bf32f48e', 0, 'Shipment created.'),
-       ('019cff8f-41e8-71d0-9431-a472bf32f48e', 1, 'Awaiting driver Sarah Smith.'),
-       ('019cff8f-41e8-71d0-9431-a472bf32f48e', 2, 'Picked up in Los Angeles.'),
-       ('019cff8f-41e8-71d0-9431-a472bf32f48e', 3, 'In transit.'),
-       ('019cff8f-41e8-71d0-9431-a472bf32f48e', 4, 'Delayed due to heavy traffic on I-10.'),
+       ('019cff8f-41e8-71d0-9431-a472bf32f48e', 0, 'Shipment created.', now() + INTERVAL '5 minutes'),
+       ('019cff8f-41e8-71d0-9431-a472bf32f48e', 1, 'Awaiting driver Sarah Smith.', now() + INTERVAL '6 minutes'),
+       ('019cff8f-41e8-71d0-9431-a472bf32f48e', 2, 'Picked up in Los Angeles.', now() + INTERVAL '7 minutes'),
+       ('019cff8f-41e8-71d0-9431-a472bf32f48e', 3, 'In transit.', now() + INTERVAL '8 minutes'),
+       ('019cff8f-41e8-71d0-9431-a472bf32f48e', 4, 'Delayed due to heavy traffic on I-10.', now() + INTERVAL '9 minutes'),
 
-       ('019cff8f-41e8-7271-a375-1c6a7a84cdf4', 0, 'Shipment created.'),
-       ('019cff8f-41e8-7271-a375-1c6a7a84cdf4', 1, 'Awaiting driver Mike Johnson.'),
-       ('019cff8f-41e8-7271-a375-1c6a7a84cdf4', 7, 'Cancelled by customer before pickup.'),
+       ('019cff8f-41e8-7271-a375-1c6a7a84cdf4', 0, 'Shipment created.', now() + INTERVAL '10 minutes'),
+       ('019cff8f-41e8-7271-a375-1c6a7a84cdf4', 1, 'Awaiting driver Mike Johnson.', now() + INTERVAL '11 minutes'),
+       ('019cff8f-41e8-7271-a375-1c6a7a84cdf4', 7, 'Cancelled by customer before pickup.', now() + INTERVAL '12 minutes'),
 
-       ('019cff8f-41e8-728b-9b77-34a660c66186', 0, 'Shipment created.'),
-       ('019cff8f-41e8-728b-9b77-34a660c66186', 1, 'Awaiting driver Elena Rodriguez.'),
-       ('019cff8f-41e8-728b-9b77-34a660c66186', 2, 'Picked up in Austin.'),
-       ('019cff8f-41e8-728b-9b77-34a660c66186', 3, 'In transit.'),
-       ('019cff8f-41e8-728b-9b77-34a660c66186', 5, 'Arrived at Dallas transfer hub.'),
+       ('019cff8f-41e8-728b-9b77-34a660c66186', 0, 'Shipment created.', now() + INTERVAL '13 minutes'),
+       ('019cff8f-41e8-728b-9b77-34a660c66186', 1, 'Awaiting driver Elena Rodriguez.', now() + INTERVAL '14 minutes'),
+       ('019cff8f-41e8-728b-9b77-34a660c66186', 2, 'Picked up in Austin.', now() + INTERVAL '15 minutes'),
+       ('019cff8f-41e8-728b-9b77-34a660c66186', 3, 'In transit.', now() + INTERVAL '16 minutes'),
+       ('019cff8f-41e8-728b-9b77-34a660c66186', 5, 'Arrived at Dallas transfer hub.', now() + INTERVAL '17 minutes'),
 
-       ('019cff8f-41e8-7297-b7b6-74ce7f1a8cf8', 0, 'Shipment created.'),
-       ('019cff8f-41e8-7297-b7b6-74ce7f1a8cf8', 1, 'Awaiting driver David Chen.'),
-       ('019cff8f-41e8-7297-b7b6-74ce7f1a8cf8', 2, 'Picked up in Atlanta.'),
+       ('019cff8f-41e8-7297-b7b6-74ce7f1a8cf8', 0, 'Shipment created.', now() + INTERVAL '18 minutes'),
+       ('019cff8f-41e8-7297-b7b6-74ce7f1a8cf8', 1, 'Awaiting driver David Chen.', now() + INTERVAL '19 minutes'),
+       ('019cff8f-41e8-7297-b7b6-74ce7f1a8cf8', 2, 'Picked up in Atlanta.', now() + INTERVAL '20 minutes'),
 
-       ('019cff8f-41e8-72a3-891a-3d59c200df16', 0, 'Shipment created.'),
-       ('019cff8f-41e8-72a3-891a-3d59c200df16', 1, 'Awaiting driver Chris Evans.'),
-       ('019cff8f-41e8-72a3-891a-3d59c200df16', 2, 'Picked up in Denver.'),
-       ('019cff8f-41e8-72a3-891a-3d59c200df16', 3, 'In transit to Salt Lake City.'),
+       ('019cff8f-41e8-72a3-891a-3d59c200df16', 0, 'Shipment created.', now() + INTERVAL '21 minutes'),
+       ('019cff8f-41e8-72a3-891a-3d59c200df16', 1, 'Awaiting driver Chris Evans.', now() + INTERVAL '22 minutes'),
+       ('019cff8f-41e8-72a3-891a-3d59c200df16', 2, 'Picked up in Denver.', now() + INTERVAL '23 minutes'),
+       ('019cff8f-41e8-72a3-891a-3d59c200df16', 3, 'In transit to Salt Lake City.', now() + INTERVAL '24 minutes'),
 
-       ('019cff8f-41e8-72ad-819d-4339dfd948d3', 0, 'Shipment created.'),
-       ('019cff8f-41e8-72ad-819d-4339dfd948d3', 1, 'Confirmed, awaiting Lisa Wong for pickup.'),
+       ('019cff8f-41e8-72ad-819d-4339dfd948d3', 0, 'Shipment created.', now() + INTERVAL '25 minutes'),
+       ('019cff8f-41e8-72ad-819d-4339dfd948d3', 1, 'Confirmed, awaiting Lisa Wong for pickup.', now() + INTERVAL '26 minutes'),
 
-       ('019cff8f-41e8-72b9-a8b3-50a5fee94c6a', 0, 'Initial request received from Nashville.'),
+       ('019cff8f-41e8-72b9-a8b3-50a5fee94c6a', 0, 'Initial request received from Nashville.', now() + INTERVAL '27 minutes'),
 
-       ('019cff8f-41e8-72c3-8fee-979d16a47ba3', 0, 'Shipment created.'),
-       ('019cff8f-41e8-72c3-8fee-979d16a47ba3', 1, 'Awaiting driver Maria Garcia.'),
-       ('019cff8f-41e8-72c3-8fee-979d16a47ba3', 2, 'Picked up.'),
-       ('019cff8f-41e8-72c3-8fee-979d16a47ba3', 3, 'In transit.'),
-       ('019cff8f-41e8-72c3-8fee-979d16a47ba3', 4, 'Weather delay in Sierra Nevada.'),
-       ('019cff8f-41e8-72c3-8fee-979d16a47ba3', 3, 'Resumed transit to Las Vegas.'),
+       ('019cff8f-41e8-72c3-8fee-979d16a47ba3', 0, 'Shipment created.', now() + INTERVAL '28 minutes'),
+       ('019cff8f-41e8-72c3-8fee-979d16a47ba3', 1, 'Awaiting driver Maria Garcia.', now() + INTERVAL '29 minutes'),
+       ('019cff8f-41e8-72c3-8fee-979d16a47ba3', 2, 'Picked up.', now() + INTERVAL '30 minutes'),
+       ('019cff8f-41e8-72c3-8fee-979d16a47ba3', 3, 'In transit.', now() + INTERVAL '31 minutes'),
+       ('019cff8f-41e8-72c3-8fee-979d16a47ba3', 4, 'Weather delay in Sierra Nevada.', now() + INTERVAL '32 minutes'),
+       ('019cff8f-41e8-72c3-8fee-979d16a47ba3', 3, 'Resumed transit to Las Vegas.', now() + INTERVAL '33 minutes'),
 
-       ('019cff8f-41e8-72cd-b1e4-47b268f143d0', 0, 'Shipment created.'),
-       ('019cff8f-41e8-72cd-b1e4-47b268f143d0', 1, 'Awaiting driver Robert Taylor.'),
-       ('019cff8f-41e8-72cd-b1e4-47b268f143d0', 2, 'Picked up in Detroit.'),
-       ('019cff8f-41e8-72cd-b1e4-47b268f143d0', 3, 'In transit.'),
-       ('019cff8f-41e8-72cd-b1e4-47b268f143d0', 6, 'Delivered to Columbus facility.');
+       ('019cff8f-41e8-72cd-b1e4-47b268f143d0', 0, 'Shipment created.', now() + INTERVAL '34 minutes'),
+       ('019cff8f-41e8-72cd-b1e4-47b268f143d0', 1, 'Awaiting driver Robert Taylor.', now() + INTERVAL '35 minutes'),
+       ('019cff8f-41e8-72cd-b1e4-47b268f143d0', 2, 'Picked up in Detroit.', now() + INTERVAL '36 minutes'),
+       ('019cff8f-41e8-72cd-b1e4-47b268f143d0', 3, 'In transit.', now() + INTERVAL '37 minutes'),
+       ('019cff8f-41e8-72cd-b1e4-47b268f143d0', 6, 'Delivered to Columbus facility.', now() + INTERVAL '38 minutes');
 -- +goose StatementEnd
 
 -- +goose Down
